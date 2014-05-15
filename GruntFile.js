a445b5ff4c43118db9f6f6c9d373b85345cdf090
army8735 <army8735@qq.com>
2014-05-15 21:15:34 +0800

    save

---
+++ GruntFile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'lib/jquery/1.10.2/jquery.js',
+        dest: 'lib/jquery/1.10.2/jquery.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
