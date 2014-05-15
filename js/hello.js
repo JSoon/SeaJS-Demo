979e2fae7db5eff67ffcb63fa80fb6f543102306
J.Soon <serdeemail@gmail.com>
2014-05-15 15:25:48 +0800

    first commit

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+define(function(require, exports, module) {
+
+    console.log("I'm in hello !")
+    var $ = require("jquery");
+    $("#hello").text("Hello Sea :)");
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+define(function(require) {
+
+    console.log("I'm in main !")
+    var hello = require("./hello");
+
+});
